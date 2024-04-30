2be16a89231626594341322595cc25f51899f711
Sean Smith <seanwessmith@gmail.com>
2024-04-30 14:15:50 -0700

    Initial Commit

---
+++ cultura/culturaApp.swift
@@ -0,0 +1,17 @@
+//
+//  culturaApp.swift
+//  cultura
+//
+//  Created by Sean Smith on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct culturaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
